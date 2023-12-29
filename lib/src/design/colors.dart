365097e779f56657c3685095eceeb5d2bca913cd
shawn-flunge <echun1234@gmail.com>
2023-12-29 18:55:57 +0900

    color파일 추가

---
+++ lib/src/design/colors.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  AppColors._();
+
+  static const Color black = Color.fromARGB(255, 32, 32, 32);
+
+  static const Color darkGrey = Color.fromARGB(255, 124, 124, 124);
+
+  static const Color grey = Color.fromARGB(255, 180, 180, 180);
+
+  static const Color borderWhite = Color.fromARGB(255, 245, 245, 245);
+
+  static const Color desktopGreen = Color.fromARGB(255, 14, 110, 110);
+}
+
+
+
